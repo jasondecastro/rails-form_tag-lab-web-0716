5069eb32839025b9b270ac4d863eea8b77fe8e25
Jason Decastro <jasonrdecastro@gmail.com>
2016-08-17 15:28:41 -0400

    finished rails form tag lab

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,14 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+    
+  end
+
+  def create
+    @data = {first_name: params[:first_name], last_name: params[:last_name]}
+    Student.create(@data)
+    redirect_to(new_student_path(student: @data))
+  end
+
 end
